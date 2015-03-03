c77e7f6f14494063d1ceedb4d5f73cf12976f1fb
dexter182 <dexter182@outlook.com>
2015-03-03 22:18:57 +0800

    完成点击，粘贴，提交的主循环，接着要写解析xml的脚本

--- policyMakeup/policyMakeup.py
+++ policyMakeup/policyMakeup.py
@@ -18,6 +18,9 @@ def paste():
     cb.CloseClipboard()
     return data
 
+def click_commit():
+    mouse.click(g.btnCommitPos[0],g.btnCommitPos[1])
+
 def click_paste(position,text):
     mouse.click(position[0],position[1])
     copy(text)
@@ -25,7 +28,33 @@ def click_paste(position,text):
     kb.tap_key('v')
     kb.release_key(kb.control_key)
 
+def init_values():
+    """ 返回一个值为空的输入项字典 """
+
+    g.boxValue = g.boxPos.copy()
+    for k in g.boxValue.keys():
+        g.boxValue[k] = ''
+    return g.boxValue
+
+def iter_paste():
+    """ 一个个进行点击-复制粘贴的主循环 """
+
+    for k,v in g.boxPos.items():
+        data = g.boxValue[k]
+        click_paste(v,data)
+        time.sleep(0.5)
+
+    click_commit()
+
 class g(object):
     boxPos = {}
+    boxValue = {}
+    btnCommitPos = []
 
-boxPos = {
+g.boxPos = {
+    # 该字典容纳保单录入各项在页面中的位置
+
+    'Voucherid':[555,100] # 单证号
+
+    }
+
