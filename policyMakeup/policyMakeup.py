19f398b585a2cb17243fa46de50dc16cbcedbe2e
dexter182 <dexter182@outlook.com>
2015-03-03 21:10:40 +0800

    准备添加文本框的坐标的字典

--- policyMakeup/policyMakeup.py
+++ policyMakeup/policyMakeup.py
@@ -1,1 +1,31 @@
-print('Hello World')
+import pymouse
+import pykeyboard
+import win32clipboard as cb
+import time
+
+mouse = pymouse.PyMouse()
+kb = pykeyboard.PyKeyboard()
+
+def copy(text):
+    cb.OpenClipboard()
+    cb.EmptyClipboard()
+    cb.SetClipboardText(text)
+    cb.CloseClipboard()
+
+def paste():
+    cb.OpenClipboard()
+    data = cb.GetClipboardData()
+    cb.CloseClipboard()
+    return data
+
+def click_paste(position,text):
+    mouse.click(position[0],position[1])
+    copy(text)
+    kb.press_key(kb.control_key)
+    kb.tap_key('v')
+    kb.release_key(kb.control_key)
+
+class g(object):
+    boxPos = {}
+
+boxPos = {
